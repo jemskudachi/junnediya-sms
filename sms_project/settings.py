5f3133679afa1673cde3966ec98c73b2d758455b
jemskudachi <jemskudachi@gmail.com>
2025-06-14 09:57:45 +0530

    settings.py

--- sms_project/settings.py
+++ sms_project/settings.py
@@ -1,1 +1,6 @@
-# Django settings placeholder
+DATABASES = {  # Replace this block if it's broken or incomplete
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
