e9a6d99082c618c309305fcf084ac2cdc7e3455d
jemskudachi <jemskudachi@gmail.com>
2025-06-14 10:10:48 +0530

    wsgi.py
    Fixed wsgi.py to add application for gunicorn

--- sms_project/wsgi.py
+++ sms_project/wsgi.py
@@ -1,1 +1,6 @@
-# Django wsgi placeholder
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_project.settings')
+
+application = get_wsgi_application()
