1408731fdf6a039d94bbb058ae913e0fbf3c9d29
jemskudachi <jemskudachi@gmail.com>
2025-06-14 10:29:18 +0530

    views.py

--- sms_project/school/views.py
+++ sms_project/school/views.py
@@ -1,1 +1,15 @@
-# views placeholder
+
+from django.shortcuts import render, get_object_or_404
+from .models import Student
+
+def bonafide_view(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'school/bonafide.html', {'student': student})
+
+def hall_ticket_view(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'school/hall_ticket.html', {'student': student})
+
+def result_view(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'school/result.html', {'student': student})
