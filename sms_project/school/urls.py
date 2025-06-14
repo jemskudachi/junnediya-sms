bd5823b4ee44e3bb6b8c7f4a2e43c56088a26a71
jemskudachi <jemskudachi@gmail.com>
2025-06-14 12:17:50 +0530

    urls.py

--- sms_project/school/urls.py
+++ sms_project/school/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from . import views
 
@@ -6,4 +5,5 @@ urlpatterns = [
     path('bonafide/<int:student_id>/', views.bonafide_view, name='bonafide'),
     path('hall_ticket/<int:student_id>/', views.hall_ticket_view, name='hall_ticket'),
     path('result/<int:student_id>/', views.result_view, name='result'),
+    path('create-admin/', views.create_admin_user),  # ✅ Add this line
 ]
