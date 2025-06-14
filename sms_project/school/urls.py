85ed400e111ce1e4acb8cdc9eba19827098b609a
jemskudachi <jemskudachi@gmail.com>
2025-06-14 10:29:44 +0530

    urls.py

--- sms_project/school/urls.py
+++ sms_project/school/urls.py
@@ -1,1 +1,9 @@
-# urls placeholder
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('bonafide/<int:student_id>/', views.bonafide_view, name='bonafide'),
+    path('hall_ticket/<int:student_id>/', views.hall_ticket_view, name='hall_ticket'),
+    path('result/<int:student_id>/', views.result_view, name='result'),
+]
