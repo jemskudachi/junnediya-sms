f6868a9531fbea2c18a18d99ab54092b0d335bd6
jemskudachi <jemskudachi@gmail.com>
2025-06-14 12:17:16 +0530

    views.py

--- sms_project/school/views.py
+++ sms_project/school/views.py
@@ -1,6 +1,7 @@
-
 from django.shortcuts import render, get_object_or_404
 from .models import Student
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def bonafide_view(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
@@ -13,3 +14,11 @@ def hall_ticket_view(request, student_id):
 def result_view(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
     return render(request, 'school/result.html', {'student': student})
+
+def create_admin_user(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        return HttpResponse("✅ Superuser created successfully.")
+    else:
+        return HttpResponse("ℹ️ Admin user already exists.")
+
