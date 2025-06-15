88f2811dfc6c1c54f6c11902498987ee2bfa0f75
jemskudachi <jemskudachi@gmail.com>
2025-06-15 12:32:11 +0530

    urls.py

--- sms_project/urls.py
+++ sms_project/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bonafide/<int:student_id>/', views.bonafide_view, name='bonafide'),
+    path('hall_ticket/<int:student_id>/', views.hall_ticket_view, name='hall_ticket'),
+    path('result/<int:student_id>/', views.result_view, name='result'),
+    path('create-admin/', views.create_admin_user),  # temporary route to create superuser
 ]
