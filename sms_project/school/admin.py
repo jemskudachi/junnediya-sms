d7a405e0bcffa3e067cb760924a88deaf6ea348f
jemskudachi <jemskudachi@gmail.com>
2025-06-15 12:51:02 +0530

    admin.py

--- sms_project/school/admin.py
+++ sms_project/school/admin.py
@@ -3,5 +3,5 @@ from .models import Student
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'class_name', 'admission_no', 'academic_year')
-    search_fields = ('full_name', 'admission_no', 'class_name')
+    list_display = ('id', 'name', 'roll_number', 'class_name', 'school', 'dob')
+    search_fields = ('name', 'roll_number', 'class_name', 'school')
