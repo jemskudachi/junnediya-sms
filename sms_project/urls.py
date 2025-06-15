2342bf770211b8579244edc24cde082c623974ca
jemskudachi <jemskudachi@gmail.com>
2025-06-15 12:59:06 +0530

    urls.py

--- sms_project/urls.py
+++ sms_project/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('school.urls')),
-    path('create-admin/', views.create_admin_user),
+    path('bonafide/<int:student_id>/', views.bonafide_view, name='bonafide'),
+    path('hall_ticket/<int:student_id>/', views.hall_ticket_view, name='hall_ticket'),
+    path('result/<int:student_id>/', views.result_view, name='result'),
+    path('create-admin/', views.create_admin_user),  # ✅ this goes here only
 ]
+
